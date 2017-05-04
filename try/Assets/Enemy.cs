54f4a33423cc57654d6cca8354c855a8c6e99d8b
KarimahAlkatiri <rima.alkatiri@gmail.com>
2017-05-04 13:49:42 -0700

    versi 1.0.0

---
+++ try/Assets/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	GameObject pathGO;
+
+	Transform targetPathNode;
+	int pathNodeIndex = 0;
+
+	float speed = 5f;
+
+	public int health = 1;
+
+	// Use this for initialization
+	void Start () {
+		pathGO = GameObject.Find("Path");
+	}
+
+	void GetNextPathNode(){
+		targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
+		pathNodeIndex++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(targetPathNode == null){
+			GetNextPathNode ();
+			if(targetPathNode == null){
+				// We've run out of path !
+				ReachedGoal();
+				return;
+			}
+		}
+
+		Vector3 dir = targetPathNode.position - this.transform.localPosition;
+
+		float distThisFrame = speed * Time.deltaTime;
+
+		if (dir.magnitude <= distThisFrame) {
+			// We reached the node
+			targetPathNode = null;
+		} 
+		else {
+			//Move towards  node
+			transform.Translate( dir.normalized * distThisFrame, Space.World );
+			this.transform.rotation = Quaternion.LookRotation(dir);
+
+		}
+
+	}
+
+	void ReachedGoal(){
+		Destroy(gameObject);
+	}
+}
